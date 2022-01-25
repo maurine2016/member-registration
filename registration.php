1ac87e1563c2b29d9022f625c0ad80bc9b86033f
maurine2016 <77607461+maurine2016@users.noreply.github.com>
2022-01-25 17:01:15 +0300

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,151 @@
+<?php
+require_once('config.php');
+// Import PHPMailer classes into the global namespace 
+use PHPMailer\PHPMailer\PHPMailer; 
+use PHPMailer\PHPMailer\Exception; 
+ 
+require 'PHPMailer/Exception.php'; 
+require 'PHPMailer/PHPMailer.php'; 
+require 'PHPMailer/SMTP.php'; 
+ 
+$mail = new PHPMailer; 
+ 
+$mail->isSMTP();                      // Set mailer to use SMTP 
+$mail->Host = 'smtp.gmail.com';       // Specify main and backup SMTP servers 
+$mail->SMTPAuth = true;               // Enable SMTP authentication 
+$mail->Username = 'user@gmail.com';   // SMTP username 
+$mail->Password = 'gmail_password';   // SMTP password 
+$mail->SMTPSecure = 'tls';            // Enable TLS encryption, `ssl` also accepted 
+$mail->Port = 587;                    // TCP port to connect to 
+ 
+// Sender info 
+$mail->setFrom('sender@codexworld.com', 'CodexWorld'); 
+$mail->addReplyTo('reply@codexworld.com', 'CodexWorld'); 
+ 
+// Add a recipient 
+$mail->addAddress('recipient@example.com'); 
+ 
+//$mail->addCC('cc@example.com'); 
+//$mail->addBCC('bcc@example.com'); 
+ 
+// Set email format to HTML 
+$mail->isHTML(true); 
+ 
+// Mail subject 
+$mail->Subject = 'Email from Localhost by CodexWorld'; 
+ 
+// Mail body content 
+$bodyContent = '<h1>How to Send Email from Localhost using PHP by CodexWorld</h1>'; 
+$bodyContent .= '<p>This HTML email is sent from the localhost server using PHP by <b>CodexWorld</b></p>'; 
+$mail->Body    = $bodyContent; 
+ 
+// Send email 
+if(!$mail->send()) { 
+    echo 'Message could not be sent. Mailer Error: '.$mail->ErrorInfo; 
+} else { 
+    echo 'Message has been sent.'; 
+} 
+ 
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Member Registration</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body>
+
+<div>
+	<?php
+	
+	?>	
+</div>
+
+<div>
+	<form action="registration.php" method="post">
+		<div class="container">
+			
+			<div class="row">
+				<div class="col-sm-3">
+					<h1>Registration</h1>
+					<p>Fill up the form with correct values.</p>
+					<hr class="mb-3">
+					<label for="firstname"><b>First Name</b></label>
+					<input class="form-control" id="firstname" type="text" name="firstname" required>
+
+					<label for="lastname"><b>Last Name</b></label>
+					<input class="form-control" id="lastname"  type="text" name="lastname" required>
+
+					<label for="email"><b>Email Address</b></label>
+					<input class="form-control" id="email"  type="email" name="email" required>
+
+					<label for="phonenumber"><b>Phone Number</b></label>
+					<input class="form-control" id="phonenumber"  type="text" name="phonenumber" required>
+
+					<label for="password"><b>Password</b></label>
+					<input class="form-control" id="password"  type="password" name="password" required>
+					<hr class="mb-3">
+					<input class="btn btn-primary" type="submit" id="register" name="create" value="Sign Up">
+				</div>
+			</div>
+		</div>
+	</form>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
+<script type="text/javascript">
+	$(function(){
+		$('#register').click(function(e){
+
+			var valid = this.form.checkValidity();
+
+			if(valid){
+
+
+			var firstname 	= $('#firstname').val();
+			var lastname	= $('#lastname').val();
+			var email 		= $('#email').val();
+			var phonenumber = $('#phonenumber').val();
+			var password 	= $('#password').val();
+			
+
+				e.preventDefault();	
+
+				$.ajax({
+					type: 'POST',
+					url: 'process.php',
+					data: {firstname: firstname,lastname: lastname,email: email,phonenumber: phonenumber,password: password},
+					success: function(data){
+					Swal.fire({
+								'title': 'Successful',
+								'text': data,
+								'type': 'success'
+								})
+							
+					},
+					error: function(data){
+						Swal.fire({
+								'title': 'Errors',
+								'text': 'There were errors while saving the data.',
+								'type': 'error'
+								})
+					}
+				});
+
+				
+			}else{
+				
+			}
+
+			
+
+
+
+		});		
+
+		
+	});
+	
+</script>
+</body>
+</html>
